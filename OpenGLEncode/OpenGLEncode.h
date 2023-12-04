5c97e3a1ed4f864f7859e72b57afccda9c8fa819
chensong <15850774503@163.com>
2023-12-04 11:04:32 +0800

    [decode]

--- OpenGLEncode/OpenGLEncode.h
+++ OpenGLEncode/OpenGLEncode.h
@@ -2,7 +2,18 @@
 
 #include <QtWidgets/QWidget>
 #include "ui_OpenGLEncode.h"
-
+#include <QtWidgets/QWidget>
+//#include "ui_OpenGLFfmpeg.h"
+#include <Windows.h>
+#include <GL/eglew.h>
+#include "QtEvent.h"
+#include "cutil.h"
+#include "camera.h"
+#include "cshader.h"
+#include "cmodel.h"
+#include "ctexture.h"
+#include "ctexture.h"
+#include "cvideo_capture.h"
 class OpenGLEncode : public QWidget
 {
     Q_OBJECT
@@ -10,7 +21,98 @@ class OpenGLEncode : public QWidget
 public:
     OpenGLEncode(QWidget *parent = nullptr);
     ~OpenGLEncode();
+    //  // 设置这个就不能使用QT渲染的了  就需要重新实现QPaintEngine函数
+//setAttribute(Qt::WA_PaintOnScreen);
+    virtual QPaintEngine* paintEngine() const { return NULL; }
+    virtual void resizeEvent(QResizeEvent* event);
+
+    void showEvent(QShowEvent* event);
+
+
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
+
+
+    void mousePressEvent(QMouseEvent* event);
+    void mouseMoveEvent(QMouseEvent* event);
+public:
+    void Renderer();
+    bool event(QEvent* event);
+
+
+    bool initializeGL();
+
 
 private:
+
+    bool CreateGLContext();
+
+    bool _gl_update();
+
+
+private slots:
+    void Tick();
+private:
     Ui::OpenGLEncodeClass ui;
+    HDC dc;
+    HGLRC rc;
+    HWND hwnd;
+
+
+
+   // unsigned char* byte;
+
+
+
+    GLuint VBO, VAO, EBO;
+
+
+
+    //GLuint program;
+    chen::ctexture* tex1; // YYYY
+    chen::ctexture* tex2; // UU
+    chen::ctexture* tex3; // VV
+
+    //GLint smp1, smp2;
+
+
+   // GLuint modelLocation;
+  //  GLuint viewLocation;
+  //  GLuint projLocation;
+
+
+    chen::Camera camera;
+
+
+    bool isPressW = false;
+    bool  isPressS = false;
+    bool  isPressA = false;
+    bool  isPressD = false;
+    bool  isPressQ = false;
+    bool  isPressE = false;
+
+
+    float speed = 1.0f;
+    float rotateSpeed = 0.2f;
+
+
+    QPoint lastPoint;
+
+
+    struct chen::Mesh* mesh = NULL;
+
+    chen::cshader1* shader = NULL;
+    //球面模型
+    chen::cmodel* quadmodel = NULL;
+
+
+    //绘制两个图形
+    chen::cshader1* shader2 = NULL;
+    // 平面模型
+    chen::cmodel* spheremodel = NULL;
+    chen::cvideo_capture* video_capture_ptr = NULL;
+
+
+    bool isVR360 = false;
+
 };
